52a90aaaac69779e7bc22ccddbddda6221cdb0b2
Sistemas Paino <88692740+victorca97@users.noreply.github.com>
2025-12-24 11:02:55 -0500

    Delete prueba.py
    "Data no valido"

--- prueba.py
+++
@@ -1,6 +0,0 @@
-def agregarcompradores(page):
-
-    page.go_back()
-    page.locator("#btnRegresar").click()
-    page.locator("#btnRegresar").click()
-    page.locator("#dgDeclaraciones_lnkPorcentaje_0 > img").click()
